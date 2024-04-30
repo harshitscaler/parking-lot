bf7e81da625e425b895920924829883dbbe53216
Harshit Sachan <sachanharshit80@gmail.com>
2024-04-30 22:22:18 +0530

    First Commit

---
+++ src/main/java/org/example/controllers/TicketController.java
@@ -0,0 +1,11 @@
+package org.example.controllers;
+
+import org.example.dtos.IssueTicketRequestDto;
+import org.example.dtos.IssueTicketResponseDto;
+
+public class TicketController {
+    public IssueTicketResponseDto issueTicket(IssueTicketRequestDto requestDto){
+        int x;
+        return null;
+    }
+}
